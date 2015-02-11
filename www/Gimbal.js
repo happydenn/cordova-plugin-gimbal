2a109a02eb212216929cab1728adb79afadbad6d
Denny Tsai <happydenn@happydenn.net>
2015-02-11 16:13:25 +0800

    Remove redundent cordova.define statement.

--- www/Gimbal.js
+++ www/Gimbal.js
@@ -1,33 +1,32 @@
-/**
+/*
+ *
  * Cordova Gimbal Plugin
+ * by Denny Tsai
+ *
  */
 
-cordova.define('io.hpd.cordova.gimbal.Gimbal', function(require, exports, module) {
+var exec = require('cordova/exec');
 
-    var exec = require('cordova/exec');
+exports.startService = function(appId, appSecret, callbackUrl, success, failed) {
+    exec(success, failed, 'Gimbal', 'startService', [appId, appSecret, callbackUrl]);
+};
 
-    exports.startService = function(appId, appSecret, callbackUrl, success, failed) {
-        exec(success, failed, 'Gimbal', 'startService', [appId, appSecret, callbackUrl]);
-    };
+exports.stopService = function() {
+    exec(function() {}, function() {}, 'Gimbal', 'stopService', []);
+};
 
-    exports.stopService = function() {
-        exec(function() {}, function() {}, 'Gimbal', 'stopService', []);
-    };
+exports.startScanSightings = function(smoothWindow) {
+    if (typeof smoothWindow === 'undefined') {
+        smoothWindow = '';
+    }
+    
+    exec(function() {}, function() {}, 'Gimbal', 'startScanSightings', [smoothWindow]);
+};
 
-    exports.startScanSightings = function(smoothWindow) {
-        if (typeof smoothWindow === 'undefined') {
-            smoothWindow = '';
-        }
-        
-        exec(function() {}, function() {}, 'Gimbal', 'startScanSightings', [smoothWindow]);
-    };
+exports.didReceiveSighting = function(success) {
+    exec(success, function() {}, 'Gimbal', 'didReceiveSighting', []);
+};
 
-    exports.didReceiveSighting = function(success) {
-        exec(success, function() {}, 'Gimbal', 'didReceiveSighting', []);
-    };
-
-    exports.stopScanSightings = function() {
-        exec(function() {}, function() {}, 'Gimbal', 'stopScanSightings', []);
-    };
-
-});
+exports.stopScanSightings = function() {
+    exec(function() {}, function() {}, 'Gimbal', 'stopScanSightings', []);
+};
